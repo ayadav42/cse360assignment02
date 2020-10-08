1face511bcd2df33ec51517695d68ecdf3cbaf2d
amarPosist <amar@posistmail.com>
2020-10-07 18:09:36 -0700

    added function bodies, main function, compiled and tested code

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -2,30 +2,54 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
-  
-  public AddingMachine () {
-    total = 0;  // not needed - included for clarity
-  }
-  
-  public int getTotal () {
-	//returns current total 
-    return 0;
-  }
-  
-  public void add (int value) {
-	//adds value to total
+  private String history;
+
+  public AddingMachine() {
+    this.total = 0; // not needed - included for clarity
+    this.history = "0";
   }
 
-  public void subtract (int value) {
-	//subtracts value from total
+  public int getTotal() {
+	  // returns the current total
+    return this.total;
   }
 
-  public String toString () {
-	//return history of transactions
-    return "";
+  public void add(int value) {
+	  // add value to the total
+    this.total = this.total + value;
+    this.history = this.history + (" + " + value);
+  }
+
+  public void subtract(int value) {
+	  // subtract value from the total
+    this.total = this.total - value;
+    this.history = this.history + (" - " + value);
+  }
+
+  public String toString() {
+	  // returns a string representing total
+    return this.history;
   }
 
   public void clear() {
-	//resets total and history
+	  // resets the total and history
+    this.total = 0;
+    this.history = "0";
+  }
+
+  public static void main(String[] args){
+    AddingMachine myCalculator = new AddingMachine();
+
+    myCalculator.add (4); 
+    System.out.println(myCalculator.getTotal());
+    myCalculator.subtract (2); 
+    System.out.println(myCalculator.getTotal());
+    myCalculator.add(5);
+    System.out.println(myCalculator.getTotal());
+
+    System.out.println(myCalculator);
+    
+    myCalculator.clear();
+    System.out.println(myCalculator);
   }
 }
