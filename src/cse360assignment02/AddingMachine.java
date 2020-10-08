a1a4d1115e481faa4205ef111b32a70532b4f178
amarPosist <amar@posistmail.com>
2020-10-07 17:51:49 -0700

    first commit

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,31 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+	//returns current total 
+    return 0;
+  }
+  
+  public void add (int value) {
+	//adds value to total
+  }
+
+  public void subtract (int value) {
+	//subtracts value from total
+  }
+
+  public String toString () {
+	//return history of transactions
+    return "";
+  }
+
+  public void clear() {
+	//resets total and history
+  }
+}
